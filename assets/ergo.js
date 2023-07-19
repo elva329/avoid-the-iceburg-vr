832e5e3148a0055782fca39f0a52045f0904ea6d
Glitch (glitch-hello-website) <none>
2023-07-19 07:36:39 +0000

    🎡👡 Checkpoint ./index.html:60002480/30659 ./assets/ergo.js:60002480/22240

--- assets/ergo.js
+++ assets/ergo.js
@@ -20,26 +20,26 @@ var player_position_index = 1;
 function movePlayerTo(position_index) {
   player_position_index = position_index;
 
-  var position = { x: 0, y: 0, z: 0 };
-  if (position_index == 0) position.x = POSITION_X_LEFT;
+  var position = {x: 0, y: 0, z: 0}
+  if      (position_index == 0) position.x = POSITION_X_LEFT;
   else if (position_index == 1) position.x = POSITION_X_CENTER;
-  else position.x = POSITION_X_RIGHT;
-  document.getElementById("player").setAttribute("position", position);
+  else                          position.x = POSITION_X_RIGHT;
+  document.getElementById('player').setAttribute('position', position);
 }
 
 /**
  * Determine how `movePlayerTo` will be fired. Use camera's rotation.
  **/
 function setupControls() {
-  AFRAME.registerComponent("lane-controls", {
+  AFRAME.registerComponent('lane-controls', {
     tick: function (time, timeDelta) {
       var rotation = this.el.object3D.rotation;
 
-      if (rotation.y > 0.1) movePlayerTo(0);
+      if      (rotation.y > 0.1)  movePlayerTo(0);
       else if (rotation.y < -0.1) movePlayerTo(2);
-      else movePlayerTo(1);
-    },
-  });
+      else                        movePlayerTo(1);
+    }
+  })
 }
 
 /*********
@@ -49,18 +49,17 @@ function setupControls() {
 var templateTreeLeft;
 var templateTreeCenter;
 var templateTreeRight;
-
 var templates;
 var treeContainer;
 var numberOfTrees = 0;
 var treeTimer;
 
 function setupTrees() {
-  templateTreeLeft = document.getElementById("template-tree-left");
-  templateTreeCenter = document.getElementById("template-tree-center");
-  templateTreeRight = document.getElementById("template-tree-right");
-  treeContainer = document.getElementById("tree-container");
-  templates = [templateTreeLeft, templateTreeCenter, templateTreeRight];
+  templateTreeLeft    = document.getElementById('template-tree-left');
+  templateTreeCenter  = document.getElementById('template-tree-center');
+  templateTreeRight   = document.getElementById('template-tree-right');
+  treeContainer       = document.getElementById('tree-container');
+  templates           = [templateTreeLeft, templateTreeCenter, templateTreeRight];
 
   removeTree(templateTreeLeft);
   removeTree(templateTreeRight);
@@ -71,17 +70,13 @@ function teardownTrees() {
   clearInterval(treeTimer);
 }
 
-function addTreesRandomlyLoop({ intervalLength = 500 } = {}) {
-  treeTimer = setInterval(addTreesRandomly, intervalLength);
-}
-
 function removeTree(tree) {
   tree.parentNode.removeChild(tree);
 }
 
 function addTree(el) {
   numberOfTrees += 1;
-  el.id = "tree-" + numberOfTrees;
+  el.id = 'tree-' + numberOfTrees;
   treeContainer.appendChild(el);
 }
 
@@ -93,34 +88,37 @@ function addTreeTo(position_index) {
 /**
  * Add any number of trees across different lanes, randomly.
  **/
-/**
- * Add any number of trees across different lanes, randomly.
- **/
-function addTreesRandomly({
-  probTreeLeft = 0.5,
-  probTreeCenter = 0.5,
-  probTreeRight = 0.5,
-  maxNumberTrees = 2,
-} = {}) {
+function addTreesRandomly(
+  {
+    probTreeLeft = 0.5,
+    probTreeCenter = 0.5,
+    probTreeRight = 0.5,
+    maxNumberTrees = 2
+  } = {}) {
+
   var trees = [
-    { probability: probTreeLeft, position_index: 0 },
-    { probability: probTreeCenter, position_index: 1 },
-    { probability: probTreeRight, position_index: 2 },
-  ];
+    {probability: probTreeLeft,   position_index: 0},
+    {probability: probTreeCenter, position_index: 1},
+    {probability: probTreeRight,  position_index: 2},
+  ]
   shuffle(trees);
 
   var numberOfTreesAdded = 0;
+  var position_indices = [];
   trees.forEach(function (tree) {
-    if (
-      Math.random() < tree.probability &&
-      numberOfTreesAdded < maxNumberTrees
-    ) {
+    if (Math.random() < tree.probability && numberOfTreesAdded < maxNumberTrees) {
       addTreeTo(tree.position_index);
       numberOfTreesAdded += 1;
+
+      position_indices.push(tree.position_index);
     }
   });
 
   return numberOfTreesAdded;
+}
+
+function addTreesRandomlyLoop({intervalLength = 500} = {}) {
+  treeTimer = setInterval(addTreesRandomly, intervalLength);
 }
 
 /**************
@@ -131,30 +129,58 @@ const POSITION_Z_OUT_OF_SIGHT = 1;
 const POSITION_Z_LINE_START = 0.6;
 const POSITION_Z_LINE_END = 0.7;
 
-function setupCollisions() {
-  AFRAME.registerComponent("player", {
-    tick: function () {
-      document.querySelectorAll(".tree").forEach(function (tree) {
-        position = tree.getAttribute("position");
-        tree_position_index = tree.getAttribute("data-tree-position-index");
-        tree_id = tree.getAttribute("id");
+AFRAME.registerComponent('player', {
+  tick: function() {
+    document.querySelectorAll('.tree').forEach(function(tree) {
+      position = tree.getAttribute('position');
+      tree_position_index = tree.getAttribute('data-tree-position-index');
+      tree_id = tree.getAttribute('id');
 
-        if (position.z > POSITION_Z_OUT_OF_SIGHT) {
-          removeTree(tree);
-        }
+      if (position.z > POSITION_Z_OUT_OF_SIGHT) {
+        removeTree(tree);
+      }
 
-        if (!isGameRunning) return;
+      if (!isGameRunning) return;
 
-        if (
-          POSITION_Z_LINE_START < position.z &&
-          position.z < POSITION_Z_LINE_END &&
-          tree_position_index == player_position_index
-        ) {
-          gameOver();
-        }
-      });
-    },
-  });
+      if (POSITION_Z_LINE_START < position.z && position.z < POSITION_Z_LINE_END
+          && tree_position_index == player_position_index) {
+        gameOver();
+      }
+
+      if (position.z > POSITION_Z_LINE_END) {
+        addScoreForTree(tree_id);
+        updateScoreDisplay();
+      }
+    })
+  }
+})
+
+/*********
+ * SCORE *
+ *********/
+
+var score;
+var countedTrees;
+var scoreDisplay;
+
+function setupScore() {
+  score = 0;
+  countedTrees = new Set();
+  scoreDisplay = document.getElementById('score');
+}
+
+function teardownScore() {
+  scoreDisplay.setAttribute('value', '');
+}
+
+function addScoreForTree(tree_id) {
+  if (countedTrees.has(tree_id)) return;
+  score += 1;
+  countedTrees.add(tree_id);
+}
+
+function updateScoreDisplay() {
+  scoreDisplay.setAttribute('value', score);
 }
 
 /********
@@ -163,8 +189,30 @@ function setupCollisions() {
 
 var isGameRunning = false;
 
-setupControls(); // TODO: AFRAME.registerComponent has to occur before window.onload?
-setupCollision();
+function gameOver() {
+  isGameRunning = false;
+
+  alert('Game Over! Refresh to start over.');
+  teardownTrees();
+  teardownScore();
+}
+
+function startGame() {
+  if (isGameRunning) return;
+  isGameRunning = true;
+
+  setupScore();
+  updateScoreDisplay();
+  addTreesRandomlyLoop();
+}
+
+setupControls();  // TODO: AFRAME.registerComponent has to occur before window.onload?
+
+window.onload = function() {
+  setupScore();
+  setupTrees();
+  startGame();
+}
 
 /*************
  * UTILITIES *
@@ -175,31 +223,12 @@ setupCollision();
  * @param {Array} a items An array containing the items.
  */
 function shuffle(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = a[i];
-    a[i] = a[j];
-    a[j] = x;
-  }
-  return a;
-}
-
-window.onload = function () {
-  setupTrees();
-   startGame();
-};
-
-function startGame() {
-  if (isGameRunning) return;
-  isGameRunning = true;
-
-  addTreesRandomlyLoop();
-}
-
-function gameOver() {
-  isGameRunning = false;
-
-  alert('Game Over!');
-  teardownTrees();
+   var j, x, i;
+   for (i = a.length - 1; i > 0; i--) {
+       j = Math.floor(Math.random() * (i + 1));
+       x = a[i];
+       a[i] = a[j];
+       a[j] = x;
+   }
+   return a;
 }
